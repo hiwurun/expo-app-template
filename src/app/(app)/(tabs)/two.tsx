aa539a3c8751323ced57ef1d0046d933dc58e079
zhoulongxi <2572642005@qq.com>
2026-01-05 11:17:24 +0800

    Initial commit

---
+++ src/app/(app)/(tabs)/two.tsx
@@ -0,0 +1,15 @@
+import { VStack } from '@nkzw/stack';
+import Text from '../../../ui/Text.tsx';
+import useViewerContext from '../../../user/useViewerContext.tsx';
+
+export default function Two() {
+  const { logout } = useViewerContext();
+
+  return (
+    <VStack flex1 padding={16}>
+      <Text onPress={logout}>
+        <fbt desc="Two header title">Logout</fbt>
+      </Text>
+    </VStack>
+  );
+}
